86990c164d2a5f730bc806ca33a9a91f816bf596
Valeria Koroleva <valeriia.koroleva@gmail.com>
2019-12-25 16:44:25 +0300

    added video section with Youtube API

--- js/main.js
+++ js/main.js
@@ -159,5 +159,104 @@ myForm.addEventListener('submit', function(e) {
         }
         myMap.geoObjects.add(myPlaceMark1).add(myPlaceMark2).add(myPlaceMark3).add(myPlaceMark4);
     }
-    //onePageScroll
+    //player
+    let player;
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('yt-player', {
+        height: "405",
+        width: "660",
+        videoId: "T_dHSP9s1BY",
+        events: {
+          onReady: onPlayerReady,
+          onStateChange: onPlayerStateChange
+        },
+        playerVars: {
+          controls: 0,
+          disablekb: 0,
+          showinfo: 0,
+          rel:0,
+          autoplay: 0,
+          modestbranding: 0
+        }
+      });
+    }
+
+    $('.player__start').on("click", e =>{
+      const btn = $(e.currentTarget);
+      
+      if (btn.hasClass("paused")){
+        player.pauseVideo();
+      } else {
+      player.playVideo();
+    }
+    }); // запуск и пауза видео со сменой соответствующей кнопки
+
+    const formatTime = timeSec => {
+      const roundTime = Math.round(timeSec);
     
+      const minutes = Math.floor(roundTime / 60);
+      const seconds = roundTime - minutes * 60;
+    
+      const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    
+      return `${minutes}:${formattedSeconds}`;
+    };    
+
+    function onPlayerReady() {
+      let interval;
+      let durationSec = player.getDuration(); // получаем длительность видео в сек
+
+      if (typeof interval !== "undefined") {
+        clearInterval(interval);
+      }
+
+      interval = setInterval(() => {
+        const completedSec = player.getCurrentTime();
+        const completedPercent = (completedSec / durationSec) *100;
+
+        $(".player__playback-button").css ({
+          left: `${completedPercent}%`
+        });
+        $(".player__duration-completed").text(formatTime(completedSec));
+      }, 1000);
+
+      $(".player__duration-estimate").text(formatTime(durationSec)); // по функции передаем длительность видео во второй таймлайн
+    }
+
+    $(".player__splash").on("click", e => {
+      player.playVideo();
+    });
+
+    $(".player__playback").on('click', e => {
+      const bar = $(e.currentTarget);
+      const newButtonPosition = e.pageX - bar.offset().left;
+      const buttonPosPercent = (newButtonPosition / bar.width()) * 100;
+      const newPlayerTimeSec = (player.getDuration() / 100) * buttonPosPercent;
+
+      player.seekTo(newPlayerTimeSec);
+      $(".player__playback-button").css({
+        left: `${buttonPosPercent}%`
+      });
+    });
+
+    function onPlayerStateChange (event) {
+      const playerButton = $('.player__start');
+      /*
+      -1 (воспроизведение видео не начато)
+      0 (воспроизведение видео завершено)
+      1 (воспроизведение)
+      2 (пауза)
+      3 (буферизация)
+      5 (видео подают реплики).
+       */
+      switch(event.data){
+        case 1:
+          $('.player__wrapper').addClass('player__wrapper--active');
+          playerButton.addClass("paused");
+          break;
+        case 2:
+          playerButton.removeClass("paused");
+          break;
+      }
+    }
+
